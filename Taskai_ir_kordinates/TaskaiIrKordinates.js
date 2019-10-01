981f59c38fe163e57c31d1d84692d739f2bb0312
uhgfel <uhgfel@gmail.com>
2019-10-01 13:35:08 +0300

    Galima pridet ir nuimpt waypointus

--- Taskai_ir_kordinates/TaskaiIrKordinates.js
+++ Taskai_ir_kordinates/TaskaiIrKordinates.js
@@ -1,64 +1,103 @@
 var TempMarker;
+var MarkerClicked;
 var MarkerNeedAdding = false;
-var CurrentMarkerIndex = 0;
+var CurrentNodeIndex = 0;
 var tempPopup;
 
-var MarkerArray = new Array();
-function Marker(marker, polyline, index){
+//TempMarker.bindPopup("<iframe src=\"./waypoint.html\"></iframe>").openPopup();
+
+var NodeArray = new Array();
+function Node(marker, polyline){
 	this.marker = marker;
 	this.polyline = polyline;
-	this.index = index;
 	this.lat = marker.getLatLng().lat;
 	this.lng = marker.getLatLng().lng;
+
+	marker.on('click', onMarkerClicked);
 }
 
+//Then map is clicked temporary marker is created
 function onMapClick(e) {
-	TempMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-	//TempMarker.bindPopup("<iframe src=\"./waypoint.html\"></iframe>").openPopup();
-	
-	//Marker events
-	TempMarker
-		.on('click', onMarkerClicked);
-		//.on('mouseover', onClick);
-	tempPopup = TempMarker.bindPopup("<button onClick=\"onAddpoint()\">Add point</button>").openPopup();
-	TempMarker.getPopup().on('remove', PopupClosedFunction);
+	CreateTempMarker(e, "<button onClick=\"onAddpoint()\">Add point</button>");
 }
 
+
+function CreateTempMarker(e, Buttons) {
+	TempMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+	tempPopup = TempMarker.bindPopup(Buttons).openPopup();
+	tempPopup.on('popupclose', function () {
+		TempMarker.remove();
+	});
+}
+
+//Then marker is clicked different temporary marker is created with Remove button
 function onMarkerClicked(e){
-	onMapClick(e);
+	MarkerClicked = e.target;
+	CreateTempMarker(e, "<button onClick=\"onAddpoint()\">Add point</button><br/><button onClick=\"onRemovePoint()\">RemovePoint</button>")
 }
 
 //Executes then "Add Point" is pressed on marker
 function onAddpoint(){
-
-	MarkerNeedAdding = true;
-
-	if(CurrentMarkerIndex > 0){
-		let apos = MarkerArray[CurrentMarkerIndex-1].marker._latlng;
-		let bpos = TempMarker.getLatLng();
-
-		var line = L.polygon([
-			[apos.lat, apos.lng],
-			[bpos.lat, bpos.lng]
-		]).addTo(map);
+	if(CurrentNodeIndex > 0){
+		var line = ConnectTwoMarkers(NodeArray[CurrentNodeIndex-1].marker, TempMarker);
 	}
 
-	var marker = new Marker(TempMarker, line, CurrentMarkerIndex);
-	MarkerArray[CurrentMarkerIndex] = marker;
+	let bpos = TempMarker.getLatLng();
+	var marker = L.marker([bpos.lat, bpos.lng]).addTo(map);
+	var node = new Node(marker, line);
+	NodeArray[CurrentNodeIndex] = node;
 
-	CurrentMarkerIndex++;
+	CurrentNodeIndex++;
 
 	tempPopup.closePopup();
 }
 
-//Pop up closed
-function PopupClosedFunction(){
-	if(!MarkerNeedAdding){
-		TempMarker.remove();
-	}
-	MarkerNeedAdding = false;
+function ConnectTwoMarkers(markerA, markerB){	
+	let apos = markerA._latlng;
+	let bpos = markerB._latlng;
+	
+	var line = L.polygon([
+		[apos.lat, apos.lng],
+		[bpos.lat, bpos.lng]
+	]).addTo(map);
+
+	return line;
 }
 
-function RemoveAllPoints(){
+function onRemovePoint(){
 
+	//Finds node to be removed
+	var nodeToRemoveIndex;
+	for(var i = 0; i < NodeArray.length; i++){
+		if(NodeArray[i].marker._latlng == MarkerClicked._latlng){
+			nodeToRemoveIndex = i;
+			break;
+		}
+	}
+
+	//Removes a line before that node if has any
+	var polyline = NodeArray[nodeToRemoveIndex].polyline;
+	if(polyline != null) polyline.remove();
+
+	//If it has a node in front of it, connects two nodes
+	if(NodeArray[nodeToRemoveIndex+1] != null){
+		NodeArray[nodeToRemoveIndex+1].polyline.remove();
+		if(NodeArray[nodeToRemoveIndex-1] != null){
+			NodeArray[nodeToRemoveIndex+1].polyline = ConnectTwoMarkers(NodeArray[nodeToRemoveIndex-1].marker, NodeArray[nodeToRemoveIndex+1].marker);
+		}
+	}
+
+	//Reorders nodes
+	NodeArray.splice(nodeToRemoveIndex, 1);
+
+	CurrentNodeIndex--;
+
+	var count = 0;
+	for(var i = 0; i < NodeArray.length; i++){
+		count++;
+	}
+	console.log(count);
+
+	MarkerClicked.remove();
+	TempMarker.remove();
 }
