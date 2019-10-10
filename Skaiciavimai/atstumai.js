ae86c77f15cdb38b254d190d2f93ed880a6fda9e
samekas1153 <sszurauskas@gmail.com>
2019-10-10 08:52:35 +0300

    555

--- Skaiciavimai/atstumai.js
+++ Skaiciavimai/atstumai.js
@@ -2,6 +2,7 @@ function Atstumai(){
     //Paspausi ant webo desini ir inspect, tada consolej matysi output
     var distan;
     for(var i = 0; i < NodeArray.length; i++){
+        console.log(i);
         if(i>0){
             NodeArray[i].latlng;
             distan=map.distance(NodeArray[i-1].latlng,NodeArray[i].latlng);
@@ -9,6 +10,7 @@ function Atstumai(){
         else{
             distan=0;
         }
-        console.log(distan/1850)
+        
+        console.log(distan/1850);
     }   
 }
