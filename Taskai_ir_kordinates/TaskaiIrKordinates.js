71ad17976e2f0884ef9c4db9034e55b10539c1ca
uhgfel <uhgfel@gmail.com>
2019-10-15 10:56:47 +0300

    Tasku dejimo sistema daugiau maziau gatava

--- Taskai_ir_kordinates/TaskaiIrKordinates.js
+++ Taskai_ir_kordinates/TaskaiIrKordinates.js
@@ -1,15 +1,21 @@
+//Used to display user there he/she clicked, but not used as permanent marker
+//"Add point" "Save point"
 var TempMarker;
+
+//Marker that user clicked. If user clicks already placed marker system will check it's index
+//by trying to match coordinates, since event parameter does not allow that (at least I don't know how to make it)
 var MarkerClicked;
-var MarkerNeedAdding = false;
+
+//Used to refference current node index that we are working with
 var CurrentNodeIndex = 0;
+
+//Popup that is opened. We asign it to the current one and close then appropriate
 var tempPopup;
 
+//Text that user wrote in popup
 var UserTextInput;
 
-//TempMarker.bindPopup("<iframe src=\"./waypoint.html\"></iframe>").openPopup();
-//
-
-//<input type=\"text\"><button onclick=\"onAddpoint()\">Add Point</button><button onclick=\"onSavePoint()\">Save Point</button>
+//Nodes define everything that user defined and objects on the map.
 var NodeArray = new Array();
 function Node(marker, polyline){
 	this.marker = marker;
@@ -23,10 +29,13 @@ function Node(marker, polyline){
 }
 
 //Then map is clicked temporary marker is created
+//"Add point", "Save point" functionality
 function onMapClick(e) {
 	CreateTempMarker(e, "<b>kordinates:</b><br/>" + e.latlng.lat + "<br/> " + e.latlng.lng + ". <br/><input id=\"Input\" type=\"text\"><br/><button onClick=\"onAddpoint()\">Add point</button><br/><button onClick=\"onSavePoint()\">SavePoint</button>");
 }
 //Then marker is clicked different temporary marker is created with Remove button, allows user to place marker on top or remove previous selection
+//"Add point" adds point on top (usefull then defining root forwards and backwards through the same path)
+//"Remove point"
 function onMarkerClicked(e){
 	MarkerClicked = e.target;
 	let name = NodeArray[GetNodeIndexByMarker(e.target._latlng)].Name;
@@ -35,6 +44,8 @@ function onMarkerClicked(e){
 }
 
 //Creates temporary marker that allows user to interact with
+//e stores click event parameters
+//buttons - define popup content, which defines types of buttons to be displayed
 function CreateTempMarker(e, Buttons) {
 	TempMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
 	tempPopup = TempMarker.bindPopup(Buttons).openPopup();
@@ -54,11 +65,14 @@ function SetTextInput(){
 function onAddpoint(){
 	SetTextInput();
 
+	SetSelectorAndAddOne(UserTextInput, CurrentNodeIndex);
+
 	PlaceNode(UserTextInput, TempMarker.getLatLng().lat, TempMarker.getLatLng().lng);
 
 	tempPopup.closePopup();
 }
 
+//Connects 2 markers with polygon line, returns line component
 function ConnectTwoMarkers(markerA, markerB){	
 	let apos = markerA._latlng;
 	let bpos = markerB._latlng;
@@ -72,6 +86,7 @@ function ConnectTwoMarkers(markerA, markerB){
 }
 
 //Compares markers from NodeArray and finds one that match coordinates with the one that was inputed
+//Returns index of that node if possible
 function GetNodeIndexByMarker(latlng){
 	for(var i = 0; i < NodeArray.length; i++){
 		if(NodeArray[i].marker._latlng == latlng){
@@ -81,25 +96,31 @@ function GetNodeIndexByMarker(latlng){
 	console.log("Not able to find a marker!");
 }
 
-//Executes then Save Button is pressed on Temp marker
+//Executes then "Save point" is pressed
 function onSavePoint(){
 	SetTextInput();
-	PointAdd(UserTextInput, TempMarker.getLatLng().lat, TempMarker.getLatLng().lng);
+
+	var lat = TempMarker.getLatLng().lat;
+	var lng = TempMarker.getLatLng().lng;
+
+	PointAdd(UserTextInput, lat, lng);
+	LoadAllSelectorsOptions();
+
 	UserTextInput = null;
 	TempMarker.remove();
 }
-//Executes then Remove point button is pressed
+//Executes then "Remove point" button is pressed
 function onRemovePoint(){
 
 	//Finds node to be removed
 	var nodeToRemoveIndex = GetNodeIndexByMarker(MarkerClicked._latlng);
 
-	RemoveNodeConnectNeighbours(nodeToRemoveIndex);
+	RemoveNode(nodeToRemoveIndex);
 
-	MarkerClicked.remove();
 	TempMarker.remove();
 }
 
+//Places node in spesific location and adds to the array of nodes
 function PlaceNode(Name, lat, lng){
 
 	UserTextInput = Name;
@@ -117,7 +138,8 @@ function PlaceNode(Name, lat, lng){
 	UserTextInput = null;
 }
 
-function RemoveNodeConnectNeighbours(index){		
+//Removes node with spesific index from map and array
+function RemoveNode(index){		
 	//Removes a line before that node if has any
 	var polyline = NodeArray[index].polyline;
 	if(polyline != null) polyline.remove();
@@ -130,19 +152,18 @@ function RemoveNodeConnectNeighbours(index){
 		}
 	}
 
+	NodeArray[index].marker.remove();
+
 	//Reorders nodes
 	NodeArray.splice(index, 1);
 
 	CurrentNodeIndex--;
+
+	RemoveSelectorLine(index);
 }
 
-//Removes line asociated with that node
-function RemoveLine(index){
-	NodeArray[index].polyline.remove();
-}
-
+//Places node in new location
 function ReplaceNode(index, newlatlng){
-	console.log(newlatlng, index);
 	//Checks if valid index is given
 	if(index > 0 || index < NodeArray.length){
 		NodeArray[index].marker._latlng = newlatlng;
@@ -150,9 +171,13 @@ function ReplaceNode(index, newlatlng){
 	}
 }
 
+//Disconnects all nodes and reconects with new order. Then we change existing node locations
+//we must redraw polygon lines correctly
 function ReorderNodes(){
-	for(var i = 1; i < NodeArray.length; i++){
+	for(let i = 1; i < NodeArray.length; i++){
 		NodeArray[i].polyline.remove();
-		ConnectTwoMarkers(NodeArray[i - 1].marker, NodeArray[i].marker);
+	}
+	for(let i = 1; i < NodeArray.length; i++){
+		NodeArray[i].polyline = ConnectTwoMarkers(NodeArray[i - 1].marker, NodeArray[i].marker);
 	}
 }
