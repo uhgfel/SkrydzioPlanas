5883cc62e92ca0615219a4ba9a4be7ec4f0a85d8
uhgfel <uhgfel@gmail.com>
2019-10-15 11:36:54 +0300

    Lektuvo pasirinkimas. Pasikeis fuel consump, speed

--- Skaiciavimai/LektuvuParametrai.js
+++ Skaiciavimai/LektuvuParametrai.js
@@ -1,4 +1,24 @@
-function AircraftSelected(e){
-    let value = e.target.value;
-    alert(value);
+//Galons per hour
+var FuelConsumption;
+
+//Knots
+var CruisingSpeed;
+
+function AircraftSelected(){
+    var SelectedValue = document.getElementById("AircraftSelector").value;
+
+    switch(SelectedValue){
+        case "C152":
+            FuelConsumption = 6;
+            CruisingSpeed = 90;
+            break;
+        case "C172":
+            FuelConsumption = 7;
+            CruisingSpeed = 100;
+            break;
+        case "CabriG2":
+            FuelConsumption = 38; //biski per daug ne?
+            CruisingSpeed = 80;
+            break;
+    }
 }
